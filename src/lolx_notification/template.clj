448327cea7004fa33b2083365ae6fc169fe69a53
trzaszcs <s.trzaszczka@wp.pl>
2017-02-23 19:46:24 +0100

    added order templates

--- src/lolx_notification/template.clj
+++ src/lolx_notification/template.clj
@@ -9,7 +9,14 @@
   {:reset {:subject "Resetowanie hasła"
            :name "reset"}
    :unread-messages {:subject "Masz nieprzeczytane wiadomości"
-                     :name "unread-messages"}})
+                     :name "unread-messages"}
+   :order-created {:subject "Nowe zamówienie"
+                     :name "order-created"}
+   :order-accepted {:subject "Zaakceptowano Twoje zamówienie"
+                   :name "order-accepted"}
+   :order-rejected {:subject "Odrzucono Twoje zamówienie"
+                    :name "order-rejected"}
+   })
 
 (defn resolve
   [type context]
